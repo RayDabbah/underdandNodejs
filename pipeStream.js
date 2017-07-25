3700729c5eee51af54fb894195ce3619dcbe86cd
raydabbah <dabbahray@yahoo.com>
2017-07-25 11:48:21 -0400

    added  more piping and gzip to pipeStream.js

--- pipeStream.js
+++ pipeStream.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
-
+const zlib = require('zlib')
 var readable = fs.createReadStream(__dirname + '/dummy.txt');
 var writable = fs.createWriteStream(__dirname + '/copydummy2.txt');
-
+var compressed = fs.createWriteStream(__dirname + '/gzDummy.txt.gz')
 readable.pipe(writable);
+
+readable.pipe(zlib.createGzip()).pipe(compressed);
+
