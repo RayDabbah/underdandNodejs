442ff8af9f3ca50eb76e86850793741ac6e52b5f
raydabbah <dabbahray@yahoo.com>
2017-07-25 22:48:11 -0400

    sending json, templating and piping all in one file on diffrent routes

--- httpServer/httpServer.js
+++ httpServer/httpServer.js
@@ -1,9 +1,24 @@
 const http = require('http')
 const fs = require('fs')
 http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-type": 'text/html' })
-    var message = 'This is without any templating engine or express!';
-    var html = fs.createReadStream(__dirname + '/httpServer.htm', 'utf8').pipe(res)
-    // html = html.replace('{Message}', message)
-    // res.end(html)
+    if (req.url === '/') {
+        res.writeHead(200, { "Content-type": 'text/html' })
+        var html = fs.createReadStream(__dirname + '/httpServer.htm', 'utf8').pipe(res)
+    }
+    if (req.url === '/template') {
+        res.writeHead(200, { "Content-type": 'text/html' })
+        var message = 'This is without any templating engine or express!';
+        var html = fs.readFileSync(__dirname + '/httpServer.htm', 'utf8')
+        html = html.replace('{Message}', message)
+        res.end(html)
+    }
+    if(req.url ==='/api'){
+         res.writeHead(200, { "Content-type": 'application/json' })
+        var obj = {
+            first: 'Avrum Shmerel',
+            last: 'Chaim Yankel'
+        }
+        res.end(JSON.stringify(obj));
+    }
+    
 }).listen(1234, 'localhost', console.log('Server is listening on port 1234'))
