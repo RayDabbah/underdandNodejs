44384a046dd934e5b753d0097fb931290453cc4d
raydabbah <dabbahray@yahoo.com>
2017-07-25 22:53:06 -0400

    added rudimentary error handling

--- httpServer/httpServer.js
+++ httpServer/httpServer.js
@@ -5,20 +5,24 @@ http.createServer(function (req, res) {
         res.writeHead(200, { "Content-type": 'text/html' })
         var html = fs.createReadStream(__dirname + '/httpServer.htm', 'utf8').pipe(res)
     }
-    if (req.url === '/template') {
+    else if (req.url === '/template') {
         res.writeHead(200, { "Content-type": 'text/html' })
         var message = 'This is without any templating engine or express!';
         var html = fs.readFileSync(__dirname + '/httpServer.htm', 'utf8')
         html = html.replace('{Message}', message)
         res.end(html)
     }
-    if(req.url ==='/api'){
+    else if(req.url ==='/api'){
          res.writeHead(200, { "Content-type": 'application/json' })
         var obj = {
             first: 'Avrum Shmerel',
             last: 'Chaim Yankel'
         }
         res.end(JSON.stringify(obj));
+    }
+    else{
+        res.writeHead(404)
+        res.end('Sorry bad day today! Hopefully better tomorrow!')
     }
     
 }).listen(1234, 'localhost', console.log('Server is listening on port 1234'))
